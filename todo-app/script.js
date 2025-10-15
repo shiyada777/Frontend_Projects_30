19bad26335a795cc51d5d3ab56c54c43a01b77a8
shiyada777 <shivakmr2025@gmail.com>
2025-10-15 17:07:28 -0500

    js code

--- todo-app/script.js
+++ todo-app/script.js
@@ -1,3 +1,18 @@
-inputBox=document.getElementById("inputBox");
-addButton=document.getElementById("addButton");
+let inputBox=document.getElementById("inputBox");
+let addButton=document.getElementById("addButton");
+let lowerPart=document.getElementById("lowerPart");
 
+
+addButton.addEventListener("click",function(){
+    let task=inputBox.value;
+
+  let container= document.createElement("div");
+container.classList.add("taskElement");
+container.innerHTML=`
+ <input type="checkbox" name="checkBox" id="checkBox">
+<p id="taskText">${task}</p>
+<button id="delete"> Delete</button>
+`;
+lowerPart.append(container);
+
+});
